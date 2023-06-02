9a2571fb8573a21759f9684798cb73e8d747c1a9
davidgarcia332 <119457787+davidgarcia332@users.noreply.github.com>
2023-06-02 19:24:57 +0200

    Add files via upload

--- test/rest/api_test.py
+++ test/rest/api_test.py
@@ -14,10 +14,60 @@ class TestApi(unittest.TestCase):
     def setUp(self):
         self.assertIsNotNone(BASE_URL, "URL no configurada")
         self.assertTrue(len(BASE_URL) > 8, "URL no configurada")
-
+    
     def test_api_add(self):
         url = f"{BASE_URL}/calc/add/2/2"
         response = urlopen(url, timeout=DEFAULT_TIMEOUT)
         self.assertEqual(
             response.status, http.client.OK, f"Error en la petición API a {url}"
+        )
+    def test_api_substract(self):
+        url = f"{BASE_URL}/calc/substract/5/2"
+        response = urlopen(url, timeout=DEFAULT_TIMEOUT)
+        self.assertEqual(
+            response.status, http.client.OK, f"Error en la petición API a {url}"
+        )
+    def test_api_multiply(self):
+        url = f"{BASE_URL}/calc/multiply/5/2"
+        response = urlopen(url, timeout=DEFAULT_TIMEOUT)
+        self.assertEqual(
+            response.status, http.client.OK, f"Error en la petición API a {url}"
+        )
+    def test_api_divide(self):
+        url = f"{BASE_URL}/calc/divide/5/2"
+        response = urlopen(url, timeout=DEFAULT_TIMEOUT)
+        self.assertEqual(
+            response.status, http.client.OK, f"Error en la petición API a {url}"
+        )
+    def test_api_divide(self):
+        url = f"{BASE_URL}/calc/divide/5/2"
+        response = urlopen(url, timeout=DEFAULT_TIMEOUT)
+        self.assertEqual(
+            response.status, http.client.OK, f"Error en la petición API a {url}"
+        )
+    def test_api_power(self):
+        url = f"{BASE_URL}/calc/power/5/2"
+        response = urlopen(url, timeout=DEFAULT_TIMEOUT)
+        self.assertEqual(
+            response.status, http.client.OK, f"Error en la petición API a {url}"
+        )
+    def test_api_square_root(self):
+        url = f"{BASE_URL}/calc/square_root/16"
+        response = urlopen(url, timeout=DEFAULT_TIMEOUT)
+        self.assertEqual(
+            response.status, http.client.OK, f"Error en la petición API a {url}"
+        )
+    def test_api_logarithm(self):
+        url = f"{BASE_URL}/calc/logarithm/100"
+        response = urlopen(url, timeout=DEFAULT_TIMEOUT)
+        self.assertEqual(
+            response.status, http.client.OK, f"Error en la petición API a {url}"
+        )
+    def test_api_logarithm(self):
+        url = f"{BASE_URL}"
+        response = urlopen(url, timeout=DEFAULT_TIMEOUT)
+        content =  response.read().decode(response.headers.get_content_charset())
+        
+        self.assertEqual(
+            content , "Hello from The Calculator!\n", f"Error en la petición API a {url}"
         )
